f30936ab714c1c111b70258f0323aeb8754cdd56
Sai Reetham Gubba <gubba.ricky@gmail.com>
2022-04-15 17:17:38 -0500

    Works with duplicated letters in word

--- functions.py
+++ functions.py
@@ -10,17 +10,16 @@ def initalSteps():
     variables.action.send_keys('roate')
     variables.action.send_keys(Keys.RETURN) 
     variables.action.perform()
-    time.sleep(4)
+    time.sleep(5)
 
 def typeInWord(word):
     variables.action.send_keys(f'{word}')
     variables.action.send_keys(Keys.RETURN)
     variables.action.perform()
-    time.sleep(4)
+    time.sleep(5)
     
 def indexes(my_list, desired_element):
     return [index for index, element in enumerate(my_list) if element == desired_element]
-
 
 def tileRowStates(word):
     i = 0
@@ -33,13 +32,11 @@ def tileRowStates(word):
             shadow_root1 = root1.shadow_root
             root2 = shadow_root1.find_element(By.CSS_SELECTOR, f"game-row[letters={word}]")
             shadow_root2 = root2.shadow_root
-            root3 = shadow_root2.find_element(By.CSS_SELECTOR, f"div:nth-child(2) > game-tile:nth-child({(indexes(word, letter)[i]) + 1})")
+            root3 = shadow_root2.find_element(By.CSS_SELECTOR, f" div:nth-child(2) > game-tile:nth-child({(indexes(word, letter)[i]) + 1})")
             shadow_root3 = root3.shadow_root
             tile = shadow_root3.find_element(By.CSS_SELECTOR, ".tile")
-            i = i + 1
-
             tileRowStates.append(tile.get_attribute("data-state"))
-
+            i = i + 1
         else:
             root1 = variables.driver.find_element(By.CSS_SELECTOR, 'game-app')
             shadow_root1 = root1.shadow_root
@@ -48,7 +45,6 @@ def tileRowStates(word):
             root3 = shadow_root2.find_element(By.CSS_SELECTOR, f"game-tile[letter={letter}]")
             shadow_root3 = root3.shadow_root
             tile = shadow_root3.find_element(By.CSS_SELECTOR, ".tile")
-
             tileRowStates.append(tile.get_attribute("data-state"))
 
     return tileRowStates
@@ -59,16 +55,14 @@ def tileRowStatesWithIndex(word):
 
     for letter in word:
 
-        if(word.count(letter) > 1):
+        if(word.count(letter) > 1): 
             root1 = variables.driver.find_element(By.CSS_SELECTOR, 'game-app')
             shadow_root1 = root1.shadow_root
             root2 = shadow_root1.find_element(By.CSS_SELECTOR, f"game-row[letters={word}]")
             shadow_root2 = root2.shadow_root
-            root3 = shadow_root2.find_element(By.CSS_SELECTOR, f"div:nth-child(2) > game-tile:nth-child({(indexes(word, letter)[i]) + 1})")
+            root3 = shadow_root2.find_element(By.CSS_SELECTOR, f" div:nth-child(2) > game-tile:nth-child({(indexes(word, letter)[i]) + 1})")
             shadow_root3 = root3.shadow_root
             tile = shadow_root3.find_element(By.CSS_SELECTOR, ".tile")
-            
-
             tileRowStatesWithIndex.append([[letter, tile.get_attribute("data-state")], indexes(word, letter)[i]])
             i = i + 1
 
@@ -80,7 +74,6 @@ def tileRowStatesWithIndex(word):
             root3 = shadow_root2.find_element(By.CSS_SELECTOR, f"game-tile[letter={letter}]")
             shadow_root3 = root3.shadow_root
             tile = shadow_root3.find_element(By.CSS_SELECTOR, ".tile")
-
             tileRowStatesWithIndex.append([[letter, tile.get_attribute("data-state")], word.find(letter)])
 
     return tileRowStatesWithIndex
@@ -94,11 +87,32 @@ def getAll5LetterWords():
     return data_into_list
 
 def logic(rowStatesWithIndex):
+
+    toCheckForDuplication = []
+    duplicationInWord = False
+    duplicatedLetter = ' '
+
+    for element in rowStatesWithIndex:
+        toCheckForDuplication.append(element[0][0])
+
+    for letter in toCheckForDuplication:
+        if(toCheckForDuplication.count(letter) > 1):
+            duplicatedLetter = letter
+            duplicationInWord = True
+            break
+
     for tileStateWithLetter in rowStatesWithIndex:
 
         if(tileStateWithLetter[0][1] == "absent"):
-            variables.absentLetters.append(tileStateWithLetter[0][0])
-        
+
+            if(duplicationInWord == False):
+                variables.absentLetters.append(tileStateWithLetter[0][0])
+            else:
+                variables.absentLetters.append(tileStateWithLetter[0][0])
+                for element in list(variables.absentLetters):
+                    if(element == duplicatedLetter):
+                        variables.absentLetters.remove(element)
+                 
         elif(tileStateWithLetter[0][1] == "present"):
             variables.letterAndIndexForPresent.append([tileStateWithLetter[0][0], tileStateWithLetter[1]])
             
@@ -112,10 +126,7 @@ def logic(rowStatesWithIndex):
                 if(word not in variables.wordsList):
                     continue
                 else:
-                    if(word not in variables.wordsList):
-                        continue
-                    else:
-                        variables.wordsList.remove(word)
+                    variables.wordsList.remove(word)
 
         for element in variables.letterAndIndexForPresent:
             if(element[0] in word):
@@ -144,13 +155,11 @@ def logic(rowStatesWithIndex):
             
 def wordleBot():
     functions.initalSteps() 
-
     logic(tileRowStatesWithIndex("roate"))
     nextWord = random.choice(variables.wordsList)
     typeInWord(nextWord)
 
     while(not(tileRowStates(nextWord) == ["correct", "correct", "correct", "correct", "correct"])):
-
         logic(tileRowStatesWithIndex(nextWord))
         nextWord = random.choice(variables.wordsList)
         typeInWord(nextWord)
